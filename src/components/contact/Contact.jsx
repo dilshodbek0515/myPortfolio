48fa8f84d807c517b6bce867934b376c53b8ecc3
dilshod <dilshodx516@gmail.com>
2025-02-11 12:15:04 +0500

    portfolio

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -16,27 +16,27 @@ const Contact = () => {
           className='w-full h-auto flex flex-col items-center gap-20'
           style={{ marginTop: '60px' }}
         >
-          <h4 className='text-7xl text-white'>Let's Talk</h4>
-          <div className='w-full flex items-center gap-10'>
+          <h4 className='text-7xl text-white max-sm:text-4xl'>Let's Talk</h4>
+          <div className='w-full flex items-center gap-10 max-xl:flex-col'>
             <input
-              className='w-full h-20 border-2 border-white rounded-2xl outline-none text-white indent-3 text-2xl'
+              className='w-full h-20 border-2 border-white rounded-2xl outline-none text-white indent-3 text-2xl max-sm:text-sm'
               placeholder='Name'
               type='text'
               required
             />
             <input
-              className='w-full h-20 border-2 border-white rounded-2xl outline-none text-white indent-3 text-2xl'
+              className='w-full h-20 border-2 border-white rounded-2xl outline-none text-white indent-3 text-2xl max-sm:text-sm'
               placeholder='Phone Number'
               type='number'
               required
             />
           </div>
           <textarea
-            className='w-full min-h-96 border-2 border-white rounded-2xl outline-none text-white text-2xl'
+            className='w-full min-h-96 border-2 border-white rounded-2xl outline-none text-white text-2xl max-sm:text-sm'
             style={{ padding: '20px' }}
             placeholder='Please contact if you have any suggestions !!!'
           ></textarea>
-          <button className='w-[50%] h-24 border-2 duration-300 cursor-pointer border-white rounded-2xl text-white text-3xl hover:bg-white hover:text-black'>
+          <button className='w-[50%] h-24 border-2 duration-300 cursor-pointer border-white rounded-2xl text-white text-3xl hover:bg-white hover:text-black max-xl:w-full'>
             Send Message
           </button>
         </form>
